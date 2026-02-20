dc75ddafc9768ffe451769bbd012e95b4b518311
Michael Tiemann <mdtiemann@gmail.com>
2026-02-20 22:23:00 +1300

    Update Electrify_GE_wages.m
    Last update before deleting file as irrelevant.

--- Electrify_GE_wages.m
+++ Electrify_GE_wages.m
@@ -4,16 +4,16 @@ function F=Electrify_GE_wages(L_h,L_f,w)
 
 F=0;
 
-if (L_h-L_f)^2 <= 10^(-6)
+if (L_h-L_f)^2 <= 10^(-9)
     return
 elseif L_f>L_h
     % If w>1, this attenuates overdemand by firms
     % If w<1, this amplifies overdemand by firms
-    F=(L_f/L_h)/w;
+    F=(L_f-L_h)/w;
 else
     % If w>1, this reverses and amplifies oversupply by households
     % If w<1, this reverses but attenuates oversupply by households
-    F=-(L_h/L_f)*w;
+    F=-(L_h-L_f)*w;
 end
 
 end
