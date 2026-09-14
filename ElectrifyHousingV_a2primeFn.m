daca4e9f506e911aef70681ad9c7c89220d5c0c4
Michael Tiemann <mdtiemann@gmail.com>
2026-09-15 05:16:17 +1200

    Vectorize ElectrifyHousingVsemiz

---
+++ ElectrifyHousingV_a2primeFn.m
@@ -0,0 +1,22 @@
+function solarpv_prime = ElectrifyHousingV_a2primeFn(installpv, solarpv)
+
+% Because we use vfoptions.refine_d, the decision variables for aprimeFn must follow the ordering d2,d3
+% Also, experience assets must be listed first in aprimeFn
+
+% 1. Create a master sizing tensor to ensure implicit expansion works safely
+master_tensor = installpv + solarpv;
+solarpv_prime = -inf(size(master_tensor), 'like', master_tensor);
+
+% 2. Condition: If installing solar for the first time
+install_mask = (installpv == 1) & (solarpv == 0);
+
+% (Safely generate a GPU-compatible matrix of random values)
+rand_sizes = cast(10 * randi([1, 4], size(master_tensor)), 'like', master_tensor);
+solarpv_prime(install_mask) = rand_sizes(install_mask);
+
+% 3. Condition: The slow degradation of installed solar capacity
+no_install_mask = (installpv == 0);
+solarpv_prime(no_install_mask) = solarpv(no_install_mask) * 0.99;
+
+
+end
---
+++ ElectrifyHousingsemizV_ReturnFn.m
@@ -0,0 +1,92 @@
+function F = ElectrifyHousingsemizV_ReturnFn(installpv, buyhouse, aprime, hprime, h, a, solarpv, ...
+    pbefore, pafter, yearsowned, olddownpayment, ...
+    z, ...
+    w, r, sigma, agej, Jr, pension, kappa_j, sigma_h, f_htc, minhouse, rentprice, houseservices, mortgageduration, pv_pct_cost, energy_pct_cost)
+
+% Note: experience asset and semi-exo asset, so first inputs are (d,a,z,...)
+% vfoptions.refine_d: only decisions d1,d3,d4 are input to ReturnFn (and this model has no d1)
+
+% --- 0. Master Tensor Setup ---
+% Add all inputs together to force MATLAB to broadcast them to the max dimension size
+master_tensor = a + h + hprime + aprime + solarpv + z + buyhouse + installpv + pbefore + pafter + yearsowned + olddownpayment;
+
+% --- 1. House and Mortgage Setup ---
+relevantdownpayment = 0.2 * buyhouse; 
+relevantdownpayment(buyhouse == 4) = olddownpayment(buyhouse == 4);
+
+housevalueatpurchase = zeros(size(master_tensor), 'like', master_tensor);
+
+mask_buy4 = (buyhouse == 4);
+housevalueatpurchase(mask_buy4) = h(mask_buy4) .* pbefore(mask_buy4);
+
+mask_buy13 = (buyhouse > 0) & (buyhouse < 4);
+housevalueatpurchase(mask_buy13) = h(mask_buy13) .* pbefore(mask_buy13) .* pafter(mask_buy13);
+
+outstandingdebt = zeros(size(master_tensor), 'like', master_tensor);
+mortgagepayment = zeros(size(master_tensor), 'like', master_tensor);
+
+own_mask = (buyhouse > 0);
+originalmortgage = zeros(size(master_tensor), 'like', master_tensor);
+originalmortgage(own_mask) = (1 - relevantdownpayment(own_mask)) .* housevalueatpurchase(own_mask);
+
+% Apply fixed exponential compounding (.^ instead of .*)
+paying_mask = own_mask & (yearsowned < 20);
+rate_factor = (1+r).^mortgageduration; 
+pmt_factor = (r .* rate_factor) ./ (rate_factor - 1);
+
+mortgagepayment(paying_mask) = originalmortgage(paying_mask) .* pmt_factor;
+outstandingdebt(paying_mask) = originalmortgage(paying_mask) .* (rate_factor - (1+r).^(yearsowned(paying_mask) + 1)) ./ (rate_factor - 1);
+
+% --- 2. Transactions and Costs ---
+costofnewhouse = zeros(size(master_tensor), 'like', master_tensor);
+move_mask = (hprime ~= h);
+costofnewhouse(move_mask) = relevantdownpayment(move_mask) .* pbefore(move_mask) .* pafter(move_mask) .* hprime(move_mask) - outstandingdebt(move_mask);
+
+htc = zeros(size(master_tensor), 'like', master_tensor);
+htc(move_mask) = f_htc * pafter(move_mask) .* hprime(move_mask);
+
+pvinstallcost = zeros(size(master_tensor), 'like', master_tensor);
+inst_mask = (installpv == 1);
+
+pv_buy0 = inst_mask & (buyhouse == 0);
+pvinstallcost(pv_buy0) = Inf;
+
+pv_buy13 = inst_mask & (buyhouse > 0) & (buyhouse < 4);
+pvinstallcost(pv_buy13) = pv_pct_cost * h(pv_buy13) .* pbefore(pv_buy13);
+
+pv_buy4 = inst_mask & (buyhouse == 4);
+pvinstallcost(pv_buy4) = 1.1 * pv_pct_cost * h(pv_buy4) .* max(pbefore(pv_buy4), pafter(pv_buy4));
+
+s = houseservices * h;
+rentalcosts = zeros(size(master_tensor), 'like', master_tensor);
+
+h_zero = (h == 0);
+s(h_zero) = 0.5 * houseservices * minhouse;
+rentalcosts(h_zero) = rentprice;
+
+% --- 3. Budget Constraint (Consumption) ---
+if agej < Jr
+    c = w * kappa_j * z + (1+r)*a - aprime - costofnewhouse - htc - rentalcosts - mortgagepayment - pvinstallcost - (energy_pct_cost * (1 - solarpv/30)); 
+else
+    c = pension + (1+r)*a - aprime - costofnewhouse - htc - rentalcosts - mortgagepayment - pvinstallcost - (energy_pct_cost * (1 - solarpv/30));
+end
+
+% --- 4. Utility Assembly ---
+F = -inf(size(master_tensor), 'like', master_tensor);
+
+valid_c = (c > 0);
+F(valid_c) = (((c(valid_c).^(1 - sigma_h)) .* (s(valid_c).^sigma_h)).^(1 - sigma)) ./ (1 - sigma);
+
+% --- 5. Invalid State Bans ---
+F((installpv == 1) & (solarpv > 0)) = -Inf;
+
+if agej >= Jr
+    F(a < 0) = -Inf;
+end
+
+F((hprime == 0) & (buyhouse ~= 0)) = -Inf;
+F((hprime ~= 0) & (hprime == h) & (buyhouse ~= 4)) = -Inf;
+F((hprime ~= 0) & (hprime ~= h) & ~(buyhouse > 0 & buyhouse < 4)) = -Inf;
+
+
+end
